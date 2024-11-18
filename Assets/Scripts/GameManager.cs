f3da4b265f2e6f54d76625cf10c85a185a7745ba
Real-pine <crewger9030@gmail.com>
2024-11-18 20:38:23 +0900

    chore: 코드수정
    Awake사용 확인

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,4 +6,9 @@ public class GameManager : MonoSingleton<GameManager>
 {
     // inherited MonoSingleton. Don't static.
     // **if you're going to use Awake method, it would be to use 'override'** By Chamsol.
+
+    public override void Awake()
+    {
+        base.Awake();
+    }
 }
