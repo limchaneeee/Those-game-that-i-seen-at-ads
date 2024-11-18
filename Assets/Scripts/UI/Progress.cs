861a47c93c97c54cc20aa59db1a1b55721aaeb2a
Hongshinyoung <gtd4095@naver.com>
2024-11-18 21:20:39 +0900

    Feat: UI진행률바
    Feat:
    - 진행률 ui

---
+++ Assets/Scripts/UI/Progress.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class Progress : MonoBehaviour
+{
+    [SerializeField] private Image progressBar;
+    [SerializeField] private TextMeshProUGUI meterTxt;
+
+    [SerializeField] private float curGauge;
+    [SerializeField] private float minGauge = 0;
+    [SerializeField] private float maxGauge = 100;
+
+    private event Action onProgress;
+
+    private void Awake()
+    {
+        progressBar = GetComponentInChildren<Image>(false);
+        meterTxt = GetComponentInChildren<TextMeshProUGUI>();
+        curGauge = minGauge;
+        onProgress += Getpercentage;
+    }
+
+    private void Getpercentage()
+    {
+        progressBar.fillAmount = curGauge / maxGauge;
+    }
+
+    private void ProgressRate()
+    {
+        if(curGauge < maxGauge)
+        {
+            curGauge += Time.deltaTime;
+            meterTxt.text = curGauge.ToString("0" + "M");
+            onProgress?.Invoke();
+        }
+        else
+        {
+            Debug.Log("스테이지 클리어");
+        }
+    }
+
+    private void Update()
+    {
+        ProgressRate();
+    }
+}
