35ed35d82ce39704b58ea0889eebcf32c21b3901
Real-pine <crewger9030@gmail.com>
2024-11-18 11:59:47 +0900

    feat: 오브젝트 풀링 구현

---
+++ Assets/Scripts/ObjectPoolManager.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Win32.SafeHandles;
+using UnityEngine;
+
+public enum ObjectPoolType
+{
+    PlayerObject,
+    EnemyObject,
+    ItemObject,
+    ObstacleObject
+}
+
+[Serializable]
+public class PoolInfo
+{
+    public ObjectPoolType type;
+    public int amount = 0;
+    public GameObject prefab;
+    public GameObject container;
+    
+    [HideInInspector]
+    public Queue<GameObject> PoolQueue = new Queue<GameObject>();
+}
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    private static ObjectPoolManager _instance;
+    public static ObjectPoolManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<ObjectPoolManager>();
+
+                if (_instance == null)
+                {
+                    GameObject singletoneObj = new GameObject(typeof(ObjectPoolManager).Name);
+                    _instance = singletoneObj.AddComponent<ObjectPoolManager>();
+                    DontDestroyOnLoad(singletoneObj);
+                }
+            }
+            return _instance;
+        }
+    }
+    
+    // 오브젝트풀 리스트
+    [SerializeField] private List<PoolInfo> listOfPool;
+        
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        InitializeObjectPool();
+    }
+
+    // 풀 리스트에 따라서 오브젝트채워주기 (초기화)
+    private void InitializeObjectPool()
+    {
+        foreach (PoolInfo poolInfo in listOfPool)
+        {
+            for (int i = 0; i < poolInfo.amount; i++)
+            {
+                poolInfo.PoolQueue.Enqueue(CreatNewObject(poolInfo));
+            }
+        }
+    }
+
+    // 오브젝트풀 안의 오브젝트 생성 메서드
+    // 생성위치는 컨테이너 기준임
+    private GameObject CreatNewObject(PoolInfo poolInfo)
+    {
+        GameObject obj = null;
+        obj = Instantiate(poolInfo.prefab, poolInfo.container.transform);
+        obj.gameObject.SetActive(false);
+
+        return obj;
+    }
+
+    // 오브젝트풀에서 꺼내 쓸 때 사용하는 메서드
+    public GameObject GetPoolObject(ObjectPoolType poolType)
+    {
+        PoolInfo selectedPool = GetPoolByType(poolType);
+
+        GameObject obj = null;
+        if (selectedPool.PoolQueue.Count > 0)
+        {
+            obj = selectedPool.PoolQueue.Dequeue();
+        }
+        else
+        {
+            obj = CreatNewObject(selectedPool);
+        }
+        obj.SetActive(true);
+        return obj;
+    }
+
+    public void GetBackObject(GameObject obj, ObjectPoolType poolType)
+    {
+        PoolInfo selectedPool = GetPoolByType(poolType);
+        selectedPool.PoolQueue.Enqueue(obj);
+        obj.SetActive(false);
+    }
+
+    // enum형식의 ObjectPoolType을 해당하는 PoolInfo에 맞게 반환해주는 함수
+    private PoolInfo GetPoolByType(ObjectPoolType poolType)
+    {
+        for (int i = 0; i < listOfPool.Count; i++)
+        {
+            if(poolType == listOfPool[i].type)
+                return listOfPool[i];
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/Test_Generate.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Generate : MonoBehaviour
+{
+    // TODO: do cashing of obj's transform.position and new WaitForSeconds for Optimization
+    
+    public void Click()
+    {
+        int random = Random.Range(0, 4);
+
+        StartCoroutine(GenerateRoutine(RandomSelect(random)));
+    }
+
+    private ObjectPoolType RandomSelect(int random)
+    {
+        if(random == 0)
+            return ObjectPoolType.PlayerObject;
+        else if (random == 1)
+            return ObjectPoolType.EnemyObject;
+        else if (random == 2)
+            return ObjectPoolType.ItemObject;
+        else
+            return ObjectPoolType.ObstacleObject;
+    }
+
+    private IEnumerator GenerateRoutine(ObjectPoolType type)
+    {
+        GameObject obj = ObjectPoolManager.Instance.GetPoolObject(type);
+
+        obj.transform.position = new Vector3(0, 0, 0);
+        obj.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(5f);
+        
+        ObjectPoolManager.Instance.GetBackObject(obj, type);
+    }
+}
