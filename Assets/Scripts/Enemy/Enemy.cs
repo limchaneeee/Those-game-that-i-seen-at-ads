c0f334f06eb3e6a1ed3388fd581596c7c5515952
limchaneeee <hypercube8878@gmail.com>
2024-11-20 11:53:48 +0900

    (Update) EnemyHpbar

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,33 +4,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, ICollisionHandler
 {
     [field: SerializeField] public EnemySO Data { get; private set; }
+    [SerializeField] private HPBar hpBar;
 
-    private float health;
+    public float health;
+
 
     private void Awake()
     {
         health = Data.EnemyData.EnemyHp;
     }
 
-    public void TakeDamage(int damage)
+    public void TakeDamage(float damage)
     {
-        health -= damage;  
+        health -= damage;
+        hpBar.UpdateHPBar();
 
         if (health <= 0)
         {
             Die();  
         }
     }
-    public void OnBulletHit()
-    {
-        TakeDamage(1);
-    }
-
     private void Die()
     {
         Destroy(gameObject);  
     }
+
+    public void OnBulletHit()
+    {
+        float playerAttackDamage = CharacterManager.Instance.Player.playerSO.shootDamage;
+        TakeDamage(playerAttackDamage);
+    }
+    public void OnPlayerHit() { }
+
+    public void OnPlayerCloneHit(GameObject obj) { }
+
+    public void OnBottomWallHit() { }
 }
---
+++ Assets/Scripts/UI/EnemyHPBar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+    [SerializeField] private Transform camPos;
+    [SerializeField] private Image fillImage;
+    [SerializeField] private Enemy enemy;
+
+    private void Start()
+    {
+        camPos = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        transform.LookAt(transform.position + camPos.rotation * Vector3.forward, camPos.rotation * Vector3.up);
+    }
+
+    public void UpdateHPBar()
+    {
+        float health = enemy.health;
+        float maxHealth = enemy.Data.EnemyData.EnemyMaxHp;
+
+        fillImage.fillAmount = health / maxHealth;
+    }
+}
