360be009c900cc90eff070f4f6df9b1255053f48
Hongshinyoung <88022654+Hongshinyoung@users.noreply.github.com>
2024-11-21 10:04:34 +0900

    Feat: 반복맵

---
+++ Assets/Scripts/Objects/MovingMap.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+public class MovingMap : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject map2;
+    private Vector3 initPosOffset = new Vector3(0, 0, 150f);
+
+    private void Start()
+    {
+        map2 = Instantiate(this.gameObject);
+        map2.transform.position = this.transform.position + initPosOffset;
+        Destroy(map2.GetComponent<MovingMap>());
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        map2.transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+
+        RePosition();
+    }
+
+    private void RePosition()
+    {
+        if (transform.position.z <= -160f)
+        {
+            transform.position += new Vector3(0, 0, 300f);
+        }
+
+        if (map2.transform.position.z <= -160f)
+        {
+            map2.transform.position += new Vector3(0, 0, 300f);
+        }
+    }
+
+}
